286ffc8522d8f0f22c47b9e018b9f5f363f17ccc
Kristin Glaze <ikglaze33@gmail.com>
2018-05-10 01:49:01 -0400

    Updated the About page with some useful information.

--- portfoliosite/client/src/components/pages/About/About.js
+++ portfoliosite/client/src/components/pages/About/About.js
@@ -3,19 +3,25 @@ import React from "react";
 const About = props => (
   <div>
     <h1>About Page</h1>
+    <h2>Web development knowledge overview</h2>
     <p>
-      Integer cursus bibendum sem non pretium. Vestibulum in aliquet sem, quis
-      molestie urna. Aliquam semper ultrices varius. Aliquam faucibus sit amet
-      magna a ultrices. Aenean pellentesque placerat lacus imperdiet efficitur.
-      In felis nisl, luctus non ante euismod, tincidunt bibendum mi. In a
-      molestie nisl, eu sodales diam. Nam tincidunt lacus quis magna posuere,
-      eget tristique dui dapibus. Maecenas fermentum elementum faucibus. Quisque
-      nec metus vestibulum, egestas massa eu, sollicitudin ipsum. Nulla
-      facilisi. Sed ut erat ligula. Nam tincidunt nunc in nibh dictum
-      ullamcorper. Class aptent taciti sociosqu ad litora torquent per conubia
-      nostra, per inceptos himenaeos. Etiam ornare rutrum felis at rhoncus.
-      Etiam vel condimentum magna, quis tempor nulla.
+      I am a full-stack web developer currently based in Raleigh, NC.
+      I work at <a href="https://www.kadro.com/" target="_blank">Kadro Solutions</a>, where I help make e-Commerce sites using the Magento 2 framework.
+      There, I mostly write code in PHP and JavaScript. I need to be able to use MySQL on a regular basis.
     </p>
+    <p>
+      Outside of work, I have attended a web development bootcamp in Raleigh by Trilogy Education Services.
+      There, I mostly learned how to write React JS apps. I became familiar with the MERN stack (Mongoose, Express, React, Node JS).
+      I also learned how to use Sequelize and deploy a React JS site to Heroku.
+    </p>
+    <h2>Education</h2>
+    <ul>
+        <li>North Carolina State University</li>
+        <ul>
+            <li>BS. in Applied Mathematics &amp; Statistics</li>
+            <li>Took several Computer Science major courses</li>
+        </ul>
+    </ul>
   </div>
 );
 
