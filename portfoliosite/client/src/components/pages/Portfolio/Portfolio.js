cf44dec6489f241f72fabdcba926a61384c92324
Kristin Glaze <ikglaze33@gmail.com>
2018-05-15 00:37:53 -0400

    Decided to refrain from making use of database to display sample sites, for now.

--- portfoliosite/client/src/components/pages/Portfolio/Portfolio.js
+++ portfoliosite/client/src/components/pages/Portfolio/Portfolio.js
@@ -24,46 +24,42 @@ class Portfolio extends Component {
             <div>
                 <h1>Portfolio Page</h1>
                 <h2>Sample sites from Web Development Bootcamp</h2>
-                {this.state.siteListings.map(siteListing => {
-                    console.log(siteListing.src);
-                    return (
-                        <SiteListing
-                        src={siteListing.src}
-                        description={siteListing.description}
-                        siteName={siteListing.siteName}
-                    />
-                    )
-                })}
                 <SiteListing
                     src="https://kiglaze.github.io/Bootstrap-Portfolio/portfolio.html"
                     description="Old version of this site."
                     siteName="Old Portfolio Site"
+                    siteContext="bootcamp"
                 />
                 <SiteListing
                     src="https://calm-mountain-94353.herokuapp.com/"
                     description="Intended to keep track of belongings. Utilizes Sequelize."
                     siteName="Find My Stuff"
+                    siteContext="bootcamp"
                 />
                 <SiteListing
                     src="https://vaijineel.github.io/BigFishSmallFish/"
                     description="Whimsical game utilizing Google Maps API and Firebase."
                     siteName="Big Fish Game"
+                    siteContext="bootcamp"
                 />
                 <SiteListing
                     src="https://kiglaze.github.io/week-4-game/"
                     description="Simple game. CSS and Javascript; nothing too fancy."
                     siteName="Gem Points Game"
+                    siteContext="bootcamp"
                 />
                 <h2>Sample sites from work at Kadro Solutions</h2>
                 <SiteListing
                     src="https://audioadvice.com"
                     description="Audio Advice"
                     siteName="Audio Advice"
+                    siteContext="kadro"
                 />
                 <SiteListing
                     src="https://www.reeds.com"
                     description="REEDS"
                     siteName="REEDS Jewelers"
+                    siteContext="kadro"
                 />
             </div>
         );
